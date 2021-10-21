3ea18f78210e7d9af9e63eb96d9e0fe516673c5c
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-10-21 12:17:14 +0200

    Render calculator components

--- src/App.js
+++ src/App.js
@@ -2,3 +2,11 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import './components/Calculator.css';
+
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App
