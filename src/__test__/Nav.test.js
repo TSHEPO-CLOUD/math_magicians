088f8dfc77d6d73ff533b63517275d4b81c27d71
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-10-29 19:55:11 +0200

    Add Nav.js test

--- src/__test__/Nav.test.js
+++ src/__test__/Nav.test.js
@@ -1,26 +1,16 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { render, screen, fireEvent } from "@testing-library/react";
-import Calculator from "../components/Calculator";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Navbar from '../Nav';
 
-describe("App", () => {
-  it("renders correctly", () => {
-    const tree = renderer.create(<Calculator />);
+describe('Navbar', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
 
     expect(tree).toMatchSnapshot();
-  });
-
-  it("Display 10.5", () => {
-    const { getByText } = render(<Calculator />);
-
-    fireEvent.click(getByText("1"));
-    fireEvent.click(getByText("6"));
-    fireEvent.click(getByText("."));
-    fireEvent.click(getByText("2"));
-    fireEvent.click(getByText("÷"));
-    fireEvent.click(getByText("2"));
-    fireEvent.click(getByText("="));
-
-    expect(getByText("8.1")).toBeInTheDocument();
   });
 });
