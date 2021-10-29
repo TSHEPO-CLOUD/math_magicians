e7a31633a48eab47f98310808bf086472921b50b
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-10-29 20:44:51 +0300

    Add quote component test

---
+++ src/__test__/pages/Quote.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Quote from "../../pages/Quote";
+
+it("should match the snapshot of quote", () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
